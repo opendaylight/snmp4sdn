e1c4662b19a47370e6aacad9f3799fa981178377
ylhsieh <ylhsieh@itri.org.tw>
2014-01-20 17:20:38 +0800

    Add copyright/license header
    Signed-off-by: ylhsieh <ylhsieh@itri.org.tw>

--- snmp4sdn/src/main/java/org/opendaylight/snmp4sdn/protocol/action/SNMPActionType.java
+++ snmp4sdn/src/main/java/org/opendaylight/snmp4sdn/protocol/action/SNMPActionType.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2013 Industrial Technology Research Institute of Taiwan and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+/*
+This code reused the code of OpenFlow Java package. The authors' efforts are appreciated.
+*/
+
 package org.opendaylight.snmp4sdn.protocol.action;
 
 
