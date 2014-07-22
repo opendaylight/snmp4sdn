bb8eef458c5f4ee0351c59b69273a32397ec0ce1
ylhsieh <ylhsieh@itri.org.tw>
2014-07-22 17:14:55 +0800

    Add the dummy case to SNMPHandler (p.s. in this commit, the adding just for those related to ReadSevice)
    Signed-off-by: ylhsieh <ylhsieh@itri.org.tw>

--- snmp4sdn/src/main/java/org/opendaylight/snmp4sdn/internal/SNMPHandler.java
+++ snmp4sdn/src/main/java/org/opendaylight/snmp4sdn/internal/SNMPHandler.java
@@ -26,8 +26,6 @@ import org.opendaylight.controller.sal.utils.Status;
 import org.opendaylight.controller.sal.utils.StatusCode;
 
 import org.opendaylight.snmp4sdn.protocol.util.HexString;
-import org.opendaylight.snmp4sdn.protocol.SNMPFlowMod;
-import org.opendaylight.snmp4sdn.internal.VLANTable;
 import org.opendaylight.snmp4sdn.internal.util.CmethUtil;
 
 import org.slf4j.Logger;
@@ -59,16 +57,11 @@ public class SNMPHandler{
     String lldpLocalPortIdOID = "1.0.8802.1.1.2.1.3.7.1.3";//s4s
     String lldpRemotePortIdOID = "1.0.8802.1.1.2.1.4.1.1.7";//s4s
 
-    String vlanNameOID = "1.3.6.1.2.1.17.7.1.4.3.1.1";
-    String vlanEgressPortsOID = "1.3.6.1.2.1.17.7.1.4.3.1.2";
-    String vlanForbiddenEgressPortsOID = "1.3.6.1.2.1.17.7.1.4.3.1.3";
-    String vlanUntaggedPortsOID = "1.3.6.1.2.1.17.7.1.4.3.1.4";
-    String vlanRowStatusOID = "1.3.6.1.2.1.17.7.1.4.3.1.5";
-    
-
     //String requestTypeOIDs = {"1.1.1.1.1"};//(when all kinds of OID more and more, may consider to use requestTypeOIDs[typeID] instead of rasing one by one like above
 
     CmethUtil cmethUtil = null;
+
+    boolean isDummy = true;
 
     public SNMPHandler(CmethUtil cmethUtil){
         this.cmethUtil = cmethUtil;
@@ -287,6 +280,16 @@ public class SNMPHandler{
     public Status sendBySNMP(Flow flow, int modType, Long sw_macAddr){
         logger.debug("enter SNMPHandler.sendBySNMP()");
 
+        if(isDummy){
+            String switchIP = cmethUtil.getIpAddr(sw_macAddr);
+            if(switchIP == null){
+                logger.error("node with mac" + sw_macAddr + "doesn't exist!");
+                return null;
+            }
+            logger.info("dummy return SUCCESS");
+            return new Status(StatusCode.SUCCESS, null);
+        }
+
         logger.debug("retrieving the metrics in the Flow...");
         //retrieve from the flow: (1)src mac (2)dest mac (3)the port value, to write into fwd table
             //to retrieve (1)&(2)
@@ -394,6 +397,17 @@ public class SNMPHandler{
     public FlowOnNode readFlowRequest(Flow flow, Node node){
         logger.debug("enter SNMPHandler.readFlowRequest()");
 
+        if(isDummy){
+            Long sw_macAddr = (Long) node.getID();
+            String switchIP = cmethUtil.getIpAddr(sw_macAddr);
+            if(switchIP == null){
+                logger.error("node with mac" + sw_macAddr + "doesn't exist!");
+                return null;
+            }
+            logger.info("dummy return the input flow as the result FlowOnNode");
+            return new FlowOnNode(flow);
+        }
+
         logger.debug("retrieving the metrics in the Flow...");
         //retrieve dest mac from the flow
         Match match = flow.getMatch();
@@ -438,6 +452,17 @@ public class SNMPHandler{
     //return value: 1. null -- switch not found  2. an empty List<FlowOnNode> -- switch found and has no entries
     public List<FlowOnNode>  readAllFlowRequest(Node node){
         logger.debug("enter SNMPHandler.readAllFlowRequest()");
+
+        if(isDummy){
+            Long sw_macAddr = (Long) node.getID();
+            String switchIP = cmethUtil.getIpAddr(sw_macAddr);
+            if(switchIP == null){
+                logger.error("node with mac" + sw_macAddr + "doesn't exist!");
+                return null;
+            }
+            logger.info("dummy return empty List<FlowOnNode>");
+            return new ArrayList<FlowOnNode>();
+        }
 
         //Use snmp to read switch fwd table...
 
@@ -736,324 +761,14 @@ public class SNMPHandler{
             }
             return table;
 
-        }
-        catch(Exception e)
-        {
-            logger.error("In readLLDPRemotePortIDEntries(), Exception during SNMP getMIBEntry:  " + e + "\n");
-            return null;
-        }
-    }
+           }
+           catch(Exception e)
+           {
+               logger.error("In readLLDPRemotePortIDEntries(), Exception during SNMP getMIBEntry:  " + e + "\n");
+               return null;
+           }
+      }
 
-    public Status addVLAN(Node node, Long vlanID){
-        logger.debug("enter SNMPHandler.addVLAN()...");
-        return addVLAN(node, vlanID, "v" + vlanID);
-    }
-    public Status addVLAN(Node node, Long vlanID, String vlanName){
-        Long sw_macAddr = (Long)(node.getID());
-        try{
-            //1. open snmp communication interface
-            String switchIP = cmethUtil.getIpAddr(sw_macAddr);
-            InetAddress sw_ipAddr = InetAddress.getByName(switchIP);
-            String community = cmethUtil.getSnmpCommunity(sw_macAddr);
-            SNMPv1CommunicationInterface comInterface = createSNMPv1CommInterface(0, sw_ipAddr, community);
-
-            //logger.debug("snmp connection created...swtich IP addr=" + sw_ipAddr.toString() + ", community=" + community);
-
-            //2. now can add vlan
-           // logger.debug("going to add vlan...");
-            boolean result = addVLANtoSwitch(comInterface, vlanID, vlanName);
-            return (result == true)?(new Status(StatusCode.SUCCESS, null)):
-                    (new Status(StatusCode.INTERNALERROR,
-                    errorString("program", "snmp to add vlan", "Vendor Extension Internal Error")));
-        }
-        catch (UnknownHostException e) {
-            logger.error("sw_macAddr {} into InetAddress.getByName() error: {}", sw_macAddr, e);
-            return new Status(StatusCode.INTERNALERROR, null);
-        }
-    }
-
-    private boolean addVLANtoSwitch(SNMPv1CommunicationInterface comInterface, Long vlanID, String vlanName){
-        logger.debug("enter SNMPHandler.addVLANtoSwitch()...");
-        String vlanIDStr = "." + vlanID;
-        /*String zeros = "000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-        String request = vlanNameOID + vlanIDStr + "s " + vlanName
-                              + vlanEgressPortsOID + vlanIDStr + "x " + zeros
-                              + vlanForbiddenEgressPortsOID + vlanIDStr + "x " + zeros
-                              + vlanUntaggedPortsOID + vlanIDStr + "x " + zeros
-                              + vlanRowStatusOID + vlanIDStr + "i 4";*/
-        byte vlanNameBytes[] = new byte[vlanName.length()];
-        try{
-            vlanNameBytes = vlanName.getBytes("US-ASCII");
-        }catch(Exception e){
-            logger.error("addVLANtoSwitch(): convert String vlanName(" + vlanName + ") to byte[] fail");
-        }
-        try{
-            String[] oids = {vlanNameOID + vlanIDStr,
-                                    vlanEgressPortsOID + vlanIDStr,
-                                    vlanForbiddenEgressPortsOID + vlanIDStr,
-                                    vlanUntaggedPortsOID + vlanIDStr,
-                                    vlanRowStatusOID + vlanIDStr
-                                    };
-            SNMPObject [] values = {
-                                    new SNMPOctetString(vlanNameBytes), 
-                                    new SNMPOctetString(new byte[42]), 
-                                    new SNMPOctetString(new byte[42]), 
-                                    new SNMPOctetString(new byte[42]), 
-                                    new SNMPInteger(4)
-                                    };
-            SNMPVarBindList newVars = comInterface.setMIBEntry(oids, values);
-            for(int i = 0; i < oids.length; i++){
-                    logger.warn("set OID {}: new value of {} = {}", oids[i], values[i].getClass().getName(), values[i]);
-            }
-        }catch(Exception e){
-            logger.error("addVLANtoSwitch, error: " + e);
-            return false;
-        }
-        return true;
-    }
-
-    public Status setVLANPorts (Node node, Long vlanID, List<NodeConnector> nodeConns){
-        logger.debug("enter SNMPHandler.setVLANPorts()...");
-        Long sw_macAddr = (Long)(node.getID());
-        try{
-            //1. open snmp communication interface
-            String switchIP = cmethUtil.getIpAddr(sw_macAddr);
-            InetAddress sw_ipAddr = InetAddress.getByName(switchIP);
-            String community = cmethUtil.getSnmpCommunity(sw_macAddr);
-            SNMPv1CommunicationInterface comInterface = createSNMPv1CommInterface(0, sw_ipAddr, community);
-
-            //logger.debug("snmp connection created...swtich IP addr=" + sw_ipAddr.toString() + ", community=" + community);
-
-            //2. now can add vlan
-            // logger.debug("going to set vlan ports...");
-            byte[] nodeConnsBytes = convertPortListToBytes(nodeConns);
-            boolean result = setVLANPortstoSwitch(comInterface, vlanID, nodeConnsBytes);
-            return (result == true)?(new Status(StatusCode.SUCCESS, null)):
-                    (new Status(StatusCode.INTERNALERROR,
-                    errorString("program", "snmp to set vlan ports", "Vendor Extension Internal Error")));
-        }
-        catch (UnknownHostException e) {
-            logger.error("sw_macAddr {} into InetAddress.getByName() error: {}", sw_macAddr, e);
-            return new Status(StatusCode.INTERNALERROR, null);
-        }
-    }
-
-    private byte[] convertPortListToBytes(List<NodeConnector> nodeConns){
-        NodeConnector nc = null;
-        int portNum = -1;
-        int portList[] = new int[48];
-        byte[] answer = new byte[6];
-        int index = 0;
-        for(int i = 0; i < nodeConns.size(); i++){
-            nc = (NodeConnector)(nodeConns.get(i));
-            portNum = ((Short)(nc.getID())).intValue() - 1;
-            portList[portNum] = 1;
-        }
-        System.out.print("port list: ");
-        for(int k = 0; k < 48; k++)
-            System.out.print(portList[k]);
-        System.out.println();
-        for(int j = 0; j < 48 - 7; j += 8){
-            int seg = portList[j] * 128 + portList[j + 1] * 64 + portList[j + 2] * 32 + portList[j + 3] * 16
-                          + portList[j + 4] * 8 + portList[j + 5] * 4 + portList[j + 6] * 2 + portList[j + 7];
-            /*int seg = portList[j] << 7 + portList[j + 1] << 6 + portList[j + 2] << 5 + portList[j + 3] << 4
-                          + portList[j + 4] << 3 + portList[j + 5] << 2 + portList[j + 6] << 1 + portList[j + 7];*/
-            logger.info("answer[" + j + "~" + (j+7) + "]=" + seg);    
-            answer[index++] = (new Integer(seg)).byteValue();
-        }
-        System.out.println("answer=" + HexString.toHexString(answer));
-        return answer;
-    }
-
-    private boolean setVLANPortstoSwitch(SNMPv1CommunicationInterface comInterface, Long vlanID, byte[] nodeConnsBytes){
-        logger.debug("enter SNMPHandler.setVLANPortstoSwitch()...");
-        try{
-            String oid = vlanEgressPortsOID + "." + vlanID;
-            byte[] value = new byte[42];
-            System.arraycopy(nodeConnsBytes, 0, value, 0, 6);
-            SNMPOctetString octetValue =  new SNMPOctetString(value);
-            comInterface.setMIBEntry(oid, octetValue);
-        }
-        catch(Exception e){
-            logger.error("In setVLANPortstoSwitch(), Exception during SNMP setMIBEntry: {}", e);
-            return false;
-        }
-        return true;
-    }
-
-    public Status deleteVLAN(Node node, Long vlanID){//return-- true:success, false:fail
-        Long sw_macAddr = (Long)(node.getID());
-        try{
-            //1. open snmp communication interface
-            String switchIP = cmethUtil.getIpAddr(sw_macAddr);
-            InetAddress sw_ipAddr = InetAddress.getByName(switchIP);
-            String community = cmethUtil.getSnmpCommunity(sw_macAddr);
-            SNMPv1CommunicationInterface comInterface = createSNMPv1CommInterface(0, sw_ipAddr, community);
-
-            //System.out.println("snmp connection created...swtich IP addr=" + sw_ipAddr.toString() + ", community=" + community);
-
-            //2. now can add vlan
-           // System.out.println("going to add vlan...");
-            boolean result = deleteVLANFromSwitch(comInterface, vlanID);
-            return (result == true)?(new Status(StatusCode.SUCCESS, null)):
-                    (new Status(StatusCode.INTERNALERROR,
-                    errorString("program", "snmp to delete vlan", "Vendor Extension Internal Error")));
-        }
-        catch (UnknownHostException e) {
-            logger.error("switchIP {} into InetAddress.getByName() error: {}", cmethUtil.getIpAddr(sw_macAddr), e);
-            return new Status(StatusCode.INTERNALERROR, null);
-        }
-    }
-
-    private boolean deleteVLANFromSwitch(SNMPv1CommunicationInterface comInterface, Long vlanID){
-        ///System.out.println("enter SNMPHandler.addVLANtoSwitch()...");
-        String vlanIDStr = "." + vlanID.toString();
-        try{
-            String oid = vlanRowStatusOID + vlanIDStr;
-            SNMPObject value = new SNMPInteger(6);
-            SNMPVarBindList newVar = comInterface.setMIBEntry(oid, value);
-            ///System.out.println("set OID " + oid + ": new value of " + value.getClass().getName() + " = " + value);
-        }catch(Exception e){
-            System.out.println("deleteVLANFromSwitch, error: " + e);
-            System.out.println("(maybe because this vlan already exists)");
-            return false;
-        }
-        return true;
-    }
-
-    public List<NodeConnector> getVLANPorts(Node node, Long vlanID){//return: the ports of the vlan on the switch
-       Long sw_macAddr = (Long)(node.getID());
-        try{
-            //1. open snmp communication interface
-            String switchIP = cmethUtil.getIpAddr(sw_macAddr);
-            InetAddress sw_ipAddr = InetAddress.getByName(switchIP);
-            String community = cmethUtil.getSnmpCommunity(sw_macAddr);
-            SNMPv1CommunicationInterface comInterface = createSNMPv1CommInterface(0, sw_ipAddr, community);
-            //System.out.println("snmp connection created...swtich IP addr=" + sw_ipAddr.toString() + ", community=" + community);
-
-            String requestOID = vlanEgressPortsOID + "." + vlanID.toString();
-            SNMPVarBindList newVars = comInterface.getMIBEntry(requestOID);
-            SNMPSequence pair = (SNMPSequence)(newVars.getSNMPObjectAt(0));
-            //SNMPObjectIdentifier snmpOID = (SNMPObjectIdentifier)pair.getSNMPObjectAt(0);
-            SNMPOctetString value = (SNMPOctetString)pair.getSNMPObjectAt(1);
-            byte[] valueBytes = (byte[])value.getValue();
-            byte[] portBytes = new byte[6];
-            System.arraycopy(valueBytes, 0, portBytes, 0, 6);
-
-            int ports[] = convertPortBytesToList(portBytes);
-
-            String portsStr = HexString.toHexString(portBytes);
-            System.out.println("to retieve switch (" + switchIP +"'s local chassis (OID: " + requestOID + "), get value: " + portsStr);
-
-            //convert ports (ex. 1010011001000... to {1,3,6,7,10})
-            int ansTmp[] = new int[48];
-            int index = 0;
-            for(int i = 0; i < 48; i++){
-                if(ports[i] !=0){
-                    ansTmp[index] = i + 1;
-                    index += 1;
-                }
-            }
-            int answer[] = new int[index];
-            System.arraycopy(ansTmp, 0, answer, 0, index);
-            return portsToNcList(answer, node);
-        }
-        catch(Exception e)
-        {
-            System.out.println("Exception during SNMP getVLANPorts: " + e);
-            return null;//meaning fail
-        }
-    }
-
-    private int[] convertPortBytesToList(byte portBytes[]){
-        if(portBytes.length != 6){
-            System.out.println("convertPortBytesToList(), input portBytes's length != 6!");
-            System.exit(0);
-        }
-        
-        int ports[] = new int[48];
-        int index = 0;
-        for(int i = 0; i < portBytes.length; i++){
-            int seg = portBytes[i] & 0xff;
-            for(int j = 8; j > 0; j--){
-                ports[index + j - 1] = seg % 2;
-                seg = seg /2;
-            }
-            index = index + 8;
-        }
-
-        return ports;
-    }
-
-    private List<NodeConnector> portsToNcList(int ports[], Node node){
-        List<NodeConnector> nodeConns = new ArrayList<NodeConnector>();
-        for(int i = 0; i < ports.length; i++){
-            nodeConns.add(createNodeConnector(new Short((short)ports[i]), node));
-        }
-        return nodeConns;
-    }
-
-    private static Node createSNMPNode(Long switchId) {
-        try {
-            return new Node("SNMP", switchId);
-        } catch (ConstructionException e1) {
-            logger.error("", e1);
-            return null;
-        }
-    }
-
-    private static NodeConnector createNodeConnector(Short portId, Node node) {
-        if (node.getType().equals("SNMP")) {
-            try {
-                return new NodeConnector("SNMP", portId, node);
-            } catch (ConstructionException e1) {
-                logger.error("",e1);
-                return null;
-            }
-        }
-        return null;
-    }
-
-    public VLANTable getVLANTable(Node node){//return: all VLANs on the switch, and also the ports of each VLAN
-        VLANTable table = new VLANTable();
-        List<NodeConnector> ports;
-
-        Long sw_macAddr = (Long)(node.getID());
-        try{
-            //1. open snmp communication interface
-            String switchIP = cmethUtil.getIpAddr(sw_macAddr);
-            InetAddress sw_ipAddr = InetAddress.getByName(switchIP);
-            String community = cmethUtil.getSnmpCommunity(sw_macAddr);
-            SNMPv1CommunicationInterface comInterface = createSNMPv1CommInterface(0, sw_ipAddr, community);
-
-            SNMPVarBindList tableVars = comInterface.retrieveMIBTable(vlanEgressPortsOID);
-            //logger.debug("Number of table entries: " + tableVars.size());
-            for(int i = 0; i < tableVars.size(); i++){
-                SNMPSequence pair = (SNMPSequence)(tableVars.getSNMPObjectAt(i));
-                SNMPObjectIdentifier snmpOID = (SNMPObjectIdentifier)pair.getSNMPObjectAt(0);
-                SNMPOctetString value = (SNMPOctetString)pair.getSNMPObjectAt(1);
-
-                String snmpOIDstr = snmpOID.toString();
-                String vlanIDstr = snmpOIDstr.substring(snmpOIDstr.lastIndexOf(".") + 1);
-                Long vlanID = Long.valueOf(vlanIDstr);
-                if(vlanID == 1) continue;
-                else{
-                    ports = getVLANPorts(createSNMPNode(sw_macAddr), vlanID);
-                }
-
-                table.addEntry(vlanID, ports);
-                //logger.debug("Retrieved OID: " + snmpOID + " (so port num=" + portNum + "), value: " + valueStr);
-            }
-            return table;
-
-        }
-        catch(Exception e)
-        {
-            System.out.println("In readLLDPLocalPortIDEntries(), Exception during SNMP getMIBEntry:  " + e + "\n");
-            return null;
-        }
-    }
-    
     private String errorString(String phase, String action, String cause) {
         return "Failed to "
                 + ((phase != null) ? phase + " the " + action
