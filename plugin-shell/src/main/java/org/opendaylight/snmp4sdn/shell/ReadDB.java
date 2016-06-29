eb0c475ba435769ee0ae90358d563eb739765393
Stephen Kitt <skitt@redhat.com>
2016-06-29 16:46:00 +0200

    Use karaf.shell instead of felix.gogo
    felix.gogo is now obsolete.
    
    Change-Id: I2d85f3a390460642e98f11d8f1c96d95038f5397
    Signed-off-by: Stephen Kitt <skitt@redhat.com>
    Signed-off-by: ylhsieh <ylhsieh@itri.org.tw>

--- plugin-shell/src/main/java/org/opendaylight/snmp4sdn/shell/ReadDB.java
+++ plugin-shell/src/main/java/org/opendaylight/snmp4sdn/shell/ReadDB.java
@@ -7,8 +7,8 @@
  */
 package org.opendaylight.snmp4sdn.shell;
 
-import org.apache.felix.gogo.commands.Argument;
-import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
 import org.opendaylight.snmp4sdn.ICore;
 
