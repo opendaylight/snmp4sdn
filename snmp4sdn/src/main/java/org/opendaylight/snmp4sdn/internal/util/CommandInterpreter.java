b8a74f3e34a0a594b98be39febaa9831021d9a32
ylhsieh <ylhsieh@itri.org.tw>
2017-07-19 17:16:01 +0800

    Fix for snmp4sdn Karaf 4 migration (2nd fix)
    (1)use odlparent 1.9.0
    we also apply gerrit #58881 to this gerrit
    
    (2)CommandInterpreter and CommandProvider is provided by org.eclipse.osgi library,
    but they are removed in this library now,
    so workaround fix them in snmp4sdn.
    
    Change-Id: I191b852098f6b8dfc3c428d8c1a6837c4243a6ce
    Signed-off-by: ylhsieh <ylhsieh@itri.org.tw>

---
+++ snmp4sdn/src/main/java/org/opendaylight/snmp4sdn/internal/util/CommandInterpreter.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017 Industrial Technology Research Institute of Taiwan and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.snmp4sdn.internal.util;
+
+public class CommandInterpreter{
+    public String nextArgument(){
+        return "(NA)";
+    }
+
+    public void println(){
+    }
+
+    public void println(String str){
+    }
+
+    public void print(){
+    }
+
+    public void print(String str){
+    }
+}
+
