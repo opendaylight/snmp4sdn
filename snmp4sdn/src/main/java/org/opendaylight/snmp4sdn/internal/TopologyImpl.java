68850f695ea9239961ad4db60ed720c3dc7085a7
ylhsieh <ylhsieh@itri.org.tw>
2017-07-19 17:16:01 +0800

    Fix for snmp4sdn Karaf 4 migration (2nd fix)
    (1)use odlparent 1.9.0
    we also apply gerrit #58881 to this gerrit
    
    (2)CommandInterpreter and CommandProvider is provided by org.eclipse.osgi library,
    but they are removed in this library now,
    so workaround fix them in snmp4sdn.
    
    Change-Id: I191b852098f6b8dfc3c428d8c1a6837c4243a6ce
    Signed-off-by: ylhsieh <ylhsieh@itri.org.tw>

--- snmp4sdn/src/main/java/org/opendaylight/snmp4sdn/internal/TopologyImpl.java
+++ snmp4sdn/src/main/java/org/opendaylight/snmp4sdn/internal/TopologyImpl.java
@@ -8,8 +8,8 @@
 
 package org.opendaylight.snmp4sdn.internal;
 
-import org.eclipse.osgi.framework.console.CommandInterpreter;
-import org.eclipse.osgi.framework.console.CommandProvider;
+import org.opendaylight.snmp4sdn.internal.util.CommandInterpreter;
+import org.opendaylight.snmp4sdn.internal.util.CommandProvider;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 
@@ -75,15 +75,15 @@ public class TopologyImpl implements TopologyService, CommandProvider{
      *
      */
     public void init() {
-        registerWithOSGIConsole();
+        //registerWithOSGIConsole();
     }
 
-    private void registerWithOSGIConsole() {
+    /*private void registerWithOSGIConsole() {
         BundleContext bundleContext = FrameworkUtil.getBundle(this.getClass())
                 .getBundleContext();
         bundleContext.registerService(CommandProvider.class.getName(), this,
                 null);
-    }
+    }*/
 
     public void setTopologyServiceShim(ITopologyServiceShim topo) {
         if(topo == null)
